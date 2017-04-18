80573740bef233de796223523812aefcbc4a0300
Jonas Lemke <jonesgit@users.noreply.github.com>
2017-04-18 18:10:23 +0200

    Getting correct lists of changed and new modules

--- test.py
+++ test.py
@@ -6,8 +6,8 @@ Created on Mon Apr 17 22:34:25 2017
 """
 import re
 
-text_new = 'Vorläufiges Gesamtergebnis\n25/120\n2,3\n2511071\nTechnische Optik\n08.03.2017\nAN (RT)\n*\nKernbereich Mechatronik\n2412281\nSimulation komplexer Systeme\n5\n1F\nBE\n1,3\nProfilbereich Mechatronik\n2412481\nElektronische Fahrzeugsysteme\n21.03.2017\n5\n1F\nBE\n2,0\n2538091\nDigitale Schaltungstechnik\n09.03.2017\n5\n1F\nBE\n2,7\n4210401\nProgrammieren I für Studierende der Mechatronik\n1F\nAN\n*\nLaborbereich B Mechatronik\n2540241\nReibungs-und Kontaktflächenphysik\n01.03.2017\n5\n1F\nBE\n2,3\n'
-text_old = 'Vorläufiges Gesamtergebnis\n15/120\n2,7\n2511071\nTechnische Optik\n08.03.2017\nAN (RT)\n*\nKernbereich Mechatronik\n2412281\nAutomatisierungstechnik 1\n(Automatisierungstechnik) \n27.02.2017\n5\n1F\nBE\n3,3\n2540103\nSimulation komplexer Systeme\n1F\nAN\n*\nProfilbereich Mechatronik\n2412481\nElektronische Fahrzeugsysteme\n21.03.2017\n1F\nAN\n*\n2538091\nDigitale Schaltungstechnik\n09.03.2017\n5\n1F\nBE\n2,7\n4210401\nProgrammieren I für Studierende der Mechatronik\n1F\nAN\n*\nLaborbereich B Mechatronik\n2540241\nReibungs-und Kontaktflächenphysik\n01.03.2017\n5\n1F\nBE\n2,3\n'
+text_new = 'Vorläufiges Gesamtergebnis\n25/120\n2,3\n2511071\nTechnische Optik\n08.03.2017\nAN (RT)\n*\nKernbereich Mechatronik\n2412281\nAutomatisierungstechnik 1\n(Automatisierungstechnik) \n27.02.2017\n5\n1F\nBE\n3,3\n2540103\nSimulation komplexer Systeme\n5\n1F\nBE\n1,3\nProfilbereich Mechatronik\n2412481\nElektronische Fahrzeugsysteme\n21.03.2017\n5\n1F\nBE\n2,0\n2538091\nDigitale Schaltungstechnik\n09.03.2017\n5\n1F\nBE\n2,7\n4210401\nProgrammieren I für Studierende der Mechatronik\n1F\nAN\n*\nLaborbereich B Mechatronik\n2540241\nReibungs-und Kontaktflächenphysik\n01.03.2017\n5\n1F\nBE\n2,3\n'
+text_old = 'Vorläufiges Gesamtergebnis\n15/120\n2,7\n2511071\nTechnische Optik\n08.03.2017\nAN (RT)\n*\nKernbereich Mechatronik\n2540103\nSimulation komplexer Systeme\n1F\nAN\n*\nProfilbereich Mechatronik\n2412481\nElektronische Fahrzeugsysteme\n21.03.2017\n1F\nAN\n*\n2538091\nDigitale Schaltungstechnik\n09.03.2017\n5\n1F\nBE\n2,7\n4210401\nProgrammieren I für Studierende der Mechatronik\n1F\nAN\n*\nLaborbereich B Mechatronik\n2540241\nReibungs-und Kontaktflächenphysik\n01.03.2017\n5\n1F\nBE\n2,3\n'
 
 lst_text_old = text_old.split(sep='\n')
 lst_text_new = text_new.split(sep='\n')
@@ -26,23 +26,37 @@ def get_modules(lst_text):
     
 def get_module_content(lst_text, start, end):
     lst_module = []
-    for value in lst_text[start:end + 1]:
+    for value in lst_text[start + 1:end + 1]:
         if not (('bereich' in value and 'Mechatronik' in value) or value == ''):
             lst_module.append(value)
     return lst_module
     
 def get_module_contents(lst_text, modules):
-    lst_module_contents = []
+    dict_module_contents = {}
     for index, tpl in enumerate(modules):
         if index < len(modules) - 1:
             end = modules[index + 1][1] - 1
         else:
             end = len(lst_text) - 1
-        lst_module_contents.append(get_module_content(lst_text, tpl[1], end))
-    return lst_module_contents
+        dict_module_contents[tpl[0]] = get_module_content(lst_text, tpl[1], end)
+    return dict_module_contents
     
+def compare_module_contents(contents_old, contents_new):
+    lst_modules_changed = []
+    lst_modules_new = []
+    for key, value in contents_new.items():
+        if key in contents_old:
+            if value != contents_old[key]:
+                lst_modules_changed.append(value)
+        else:
+            lst_modules_new.append(value)
+    return lst_modules_changed, lst_modules_new
+        
 modules_old = get_modules(lst_text_old)
 modules_new = get_modules(lst_text_new)
 
-module_contents_old = get_module_contents(lst_text_old, modules_old)
-module_contents_new = get_module_contents(lst_text_new, modules_new)
+dict_module_contents_old = get_module_contents(lst_text_old, modules_old)
+dict_module_contents_new = get_module_contents(lst_text_new, modules_new)
+
+lst_modules_changed, lst_modules_new = \
+    compare_module_contents(dict_module_contents_old, dict_module_contents_new)
