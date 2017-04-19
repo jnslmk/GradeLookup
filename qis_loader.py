d8bbc25ebf096a1f8f381f90785c77fb1639b995
Jonas Lemke <jonesgit@users.noreply.github.com>
2017-04-19 11:46:36 +0200

    merged functions from test.py resulting and finished script

--- qis_loader.py
+++ qis_loader.py
@@ -51,9 +51,9 @@ def text_from_pdf(filestream):
 def compare_pdfs(oldfile, newfile):
     if os.path.isfile(FILENAME):
         with open(oldfile, 'rb') as f:
-            pdf_text_body_old = text_from_pdf(f)
+            pdf_text_body_old = text_from_pdf(f)[0]
         with open(newfile, 'rb') as f:
-            pdf_text_body_new = text_from_pdf(f)
+            pdf_text_body_new = text_from_pdf(f)[0]
         if pdf_text_body_old == pdf_text_body_new:
             print('No changes since last download')
             return 0, pdf_text_body_old, pdf_text_body_new
@@ -64,11 +64,95 @@ def compare_pdfs(oldfile, newfile):
         print('No previous file found')
         return 0, 0, 0
         
-def get_total_result(text):
-    re.findall()
-        
-def get_differences(text_old, text_new):
-    return 1
+def replace_pdf(old_pdf, replacement):
+    os.remove(old_pdf)
+    os.rename(replacement, old_pdf)
+    
+def get_dict_total(lst_text):
+    dict_total = {}
+    dict_total['total_credits'] = lst_text[1]
+    dict_total['total_grade'] = lst_text[2]
+    return dict_total
+
+def get_modules(lst_text):
+    lst_modules = []
+    for index, value in enumerate(lst_text):
+        if re.match(r'\d{7}', value):
+            lst_modules.append([value, index])
+    return lst_modules
+    
+def get_module_content(lst_text, start, end):
+    lst_module = []
+    for value in lst_text[start + 1:end + 1]:
+        if not (('bereich' in value and 'Mechatronik' in value) or value == ''):
+            lst_module.append(value)
+    return lst_module
+    
+def get_module_contents(lst_text, modules):
+    dict_module_contents = {}
+    for index, tpl in enumerate(modules):
+        if index < len(modules) - 1:
+            end = modules[index + 1][1] - 1
+        else:
+            end = len(lst_text) - 1
+        dict_module_contents[tpl[0]] = get_module_content(lst_text, tpl[1], end)
+    return dict_module_contents
+    
+def compare_module_contents(contents_old, contents_new):
+    lst_modules_changed = []
+    lst_modules_new = []
+    for key, value in contents_new.items():
+        if key in contents_old:
+            if value != contents_old[key]:
+                lst_modules_changed.append([value, contents_old[key]])
+        else:
+            lst_modules_new.append(value)
+    return lst_modules_changed, lst_modules_new
+    
+def build_mailtext(dt_old, dt_new, lst_changed, lst_new):
+    text = ''
+    if dt_old['total_credits'] != dt_new['total_credits'] \
+            and dt_old['total_grade'] != dt_new['total_grade']:
+        text += 'Neue Gesamtnote: ' + dt_new['total_grade'] + ' bei ' \
+                    + dt_new['total_credits'] + ',\n' \
+                    + 'vorher ' + dt_old['total_grade'] + ' bei ' \
+                    + dt_old['total_credits'] + '.\n\n'
+    else:
+        text += 'Unveränderte Gesamtnote ' + dt_new['total_grade'] \
+                    + ' bei ' + dt_new['total_credits'] + '.\n\n'
+    if len(lst_new) != 0:
+        text += 'Neu hinzugefügte Module:\n'
+        for module in lst_new:
+            for entry in module[:-1]:
+                text += entry + ' '
+            text += module[-1] + '\n'
+        text += '\n'
+    if len(lst_changed[0]) != 0:
+        text += 'Veränderte Module:\n'
+        for module in lst_changed:
+            for entry in module[0][:-1]:
+                text += entry + ' '
+            text += module[0][-1] + '\n'
+            text += '('
+            for entry in module[1][:-1]:
+                text += entry + ' '
+            text += module[1][-1] + ')\n'
+    return text
+    
+def send_notification(text, pdf_file, email, recepient, api_key):
+    p = requests.post(
+        "https://api.mailgun.net/v3/sandboxf9f20535651b426ab55ebc05468cd3f1.mailgun.org/messages",
+        auth=("api", api_key),
+        data={"from": "Mailgun Sandbox <postmaster@sandboxf9f20535651b426ab55ebc05468cd3f1.mailgun.org>",
+              "to": recepient + ' <' + email + '>',
+              "subject": "Neue Moduleinträge im QIS-Portal",
+              "text": text},
+        files=[("attachment", open(pdf_file, 'rb'))]
+        )
+    if p.status_code == 200:
+        print('Sent email successfully')
+    else:
+        print('Email sending not successful')
 
 FILENAME = 'studienverlauf.pdf'
 FILENAME_TMP = 'tmp.pdf'
@@ -85,7 +169,23 @@ api_key = config['email account']['api-key']
 try:
     download_gradesheet(FILENAME_TMP, username, password)
     changed, text_old, text_new = compare_pdfs(FILENAME, FILENAME_TMP)
+    replace_pdf(FILENAME, FILENAME_TMP)
     if changed:
-        differences = get_differences(text_old, text_new)
+        #Text handling
+        lst_text_old = text_old.split(sep='\n')
+        lst_text_new = text_new.split(sep='\n')
+        dict_total_old = get_dict_total(lst_text_old)
+        dict_total_new = get_dict_total(lst_text_new)
+        modules_old = get_modules(lst_text_old)
+        modules_new = get_modules(lst_text_new)
+        dict_module_contents_old = get_module_contents(lst_text_old, modules_old)
+        dict_module_contents_new = get_module_contents(lst_text_new, modules_new)
+        lst_modules_changed, lst_modules_new = compare_module_contents(
+            dict_module_contents_old, dict_module_contents_new)
+        #mail handling
+        mailtext = build_mailtext(dict_total_old, dict_total_new, \
+                          lst_modules_changed, lst_modules_new)
+        send_notification(mailtext, FILENAME, email, recepient, api_key)
+    
 except SystemExit:
     print('Script aborted.')
